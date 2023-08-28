26fbc2c3632e0195f69c6def86d1d57628c3ab78
PrajwalK7 <prajwalkhajure7@gmail.com.com>
2023-08-28 11:24:47 +0530

    28-aug Palindrome number and palindrome string

--- Java learning/Hello.java
+++ Java learning/Hello.java
@@ -7,12 +7,12 @@ public class Hello {
         /*
          * Link : https://www.youtube.com/watch?v=3vbizK6KzaU&list=PLUDwpEzHYYLtgkXK6YaZ4I2XcsjMqIaEa
          */
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter value of a ");
-        int a = sc.nextInt();
-        System.out.println("Enter value of b ");
-        int b = sc.nextInt();
-        System.out.println("Before swap value of a " + a + "value of b " + b);
+        //Scanner sc = new Scanner(System.in);
+        //System.out.println("Enter value of a ");
+        //int a = sc.nextInt();
+        //System.out.println("Enter value of b ");
+        //int b = sc.nextInt();
+        //System.out.println("Before swap value of a " + a + "value of b " + b);
         //swappingOne(a, b);// using third variable
         //swappingTwo(a, b);//without using third variable addition subtraction
         //swappingThree(a, b);//without using third variable mul/div
@@ -23,14 +23,35 @@ public class Hello {
          * link : https://www.youtube.com/watch?v=m0lMpNIG6vU&list=PLUDwpEzHYYLtgkXK6YaZ4I2XcsjMqIaEa&index=2
          * 
         */
-        System.out.println("Algo Reverse of the number is " + reverseNumberAlgo(1020));
-        reverseNumberStrBuffer(201);
-        reverseNumberStrBuilder(201);
+        //System.out.println("Algo Reverse of the number is " + reverseNumberAlgo(1020));
+        //reverseNumberStrBuffer(201);
+        //reverseNumberStrBuilder(201);
         //2reverse string 
-        reverseStrAlgoString("ABCV"); // String
-        reverseStrAlgoCharArr("ABCV"); // char array
-        reverseStrBuf("ABCD");
+        //reverseStrAlgoString("ABCV"); // String
+        //reverseStrAlgoCharArr("ABCV"); // char array
+        //reverseStrBuf("ABCD");
         /*===Day 2 :27-Aug end========*/
+
+        /*===Day 3 :28-Aug Start========
+         * link : https://www.youtube.com/watch?v=kNE3vq1g2e8&list=PLUDwpEzHYYLtgkXK6YaZ4I2XcsjMqIaEa&index=4
+         * link : https://www.youtube.com/watch?v=8BlUi4-4N4E&list=PLUDwpEzHYYLtgkXK6YaZ4I2XcsjMqIaEa&index=5
+         * 
+        */
+        boolean isPlain = palindromeNumber(121);
+        if(isPlain){
+            System.out.println("yes palindrome");
+        }else{
+             System.out.println("no palindrome");
+        }
+
+        boolean isPalStr = palindromeString("nitin");
+        if(isPalStr){
+            System.out.println("yes palindrome");
+        }else{
+             System.out.println("no palindrome");
+        }
+        /*===Day 3 :27-Aug end========*/
+
     }
 
     public static void swappingOne(int a, int b) {
@@ -96,5 +117,31 @@ public class Hello {
     public static void reverseStrBuf(String str){
         StringBuffer sb = new StringBuffer(str);
         System.out.println("reverse using string buffer--> " + sb.reverse());
+    }
+    public static boolean palindromeNumber(int num){
+        
+        int rev=0;
+        int ori = num;
+        while(num!=0){
+            rev = num%10  + rev *10;
+            num=num/10;
+        }
+        if(rev==ori){
+            return true;
+        }
+        return false;
+
+    }
+    public static boolean palindromeString(String str) {
+        //String ori = str;
+        System.out.println("str function callsed");
+        String rev = "";
+        for(int i=str.length()-1; i>=0; i--){
+            rev = rev+ str.charAt(i);
+        }
+        if(rev.equalsIgnoreCase(str) ){
+            return true;
+        }
+        return false;
     }
 }
